8d61a82f8af6b7c4e650bca4b23fe8b769112013
Manuel Lépori <mlepori@gmail.com>
2013-03-25 10:08:00 -0300

    Primer ejemplo sobre herencia y overriding

--- test.php
+++ test.php
@@ -3,24 +3,23 @@
 	
 	
 
-	$pructo = new ShopProduct("Rayuela", "Pedro", "Cortaza", "$9.99");
+	$producto = new ShopProduct("Rayuela", "Pedro", "Cortaza", "$9.99");
 	
 	$cd = new CdProduct("Los twists", "Los", "Twists", "10.99", "1:20:00");
 
+	$libro = new BookProduct ("La invencion de Morel", "Adolfo", "Bioy Casares", "9.99", "1200 paginas");
+
+
 	
-	print $cd->getPlayLength();
+	print ($producto->getSummaryLine());
 	
+	echo "<BR>";
 	
+	print ($cd->getSummaryLine());
 	
+	echo "<BR>";	
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	print ($libro->getSummaryLine());
 	
 	
 	
