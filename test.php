0c2bf9602ac9c622df657793f894a4a004c297ee
Manuel Lépori <mlepori@gmail.com>
2013-03-27 13:34:45 -0300

    Se agrego un metodo estatico

--- test.php
+++ test.php
@@ -1,31 +1,25 @@
 <?php
 	require 'ShopProduct.class.inc';
+	require 'ShopProductWriter.class.inc';
+	require_once("DB.php");	
+	
+	
+	$db = DB::connect("mysql://root:root@localhost:8889/store");
+	$obj = ShopProduct::getInstance(4, $db);
+	
 	
 	
 
-	$producto = new ShopProduct("Rayuela", "Pedro", "Cortaza", "$9.99");
-	
-	$cd = new CdProduct("Los twists", "Los", "Twists", "10.99", "1:20:00");
-
-	$libro = new BookProduct ("La invencion de Morel", "Adolfo", "Bioy Casares", "9.99", "1200 paginas");
 
 
-	
-	print ($producto->getSummaryLine());
-	
-	echo "<BR>";
-	
-	print ($cd->getSummaryLine());
-	
-	echo "<BR>";	
-	
-	print ($libro->getSummaryLine());
-	
+
 	
 	
 /**----
 
+$cd = new CdProduct("Los twists", "Los", "Twists", "10.99", "1:20:00");
 
+$libro = new BookProduct ("La invencion de Morel", "Adolfo", "Bioy Casares", "9.99", "1200 paginas");
 
 	print "author: " . $product1->getProducer() . "\n";
 
@@ -38,7 +32,20 @@ $product1 = new ShopProduct("La invencion de Morel", "Adolfo Bioy", "Casares", 9
 $writer = new ShopProductWriter();
 $writer->write($product1);
 
+
+
+$libro_rayuela = new BookProduct("Rayuela", null, null, "19.99", null, "1200 paginas");
+
+
+$clase_write = new ShopProductWriter();
+$clase_write->addProduct($libro_rayuela);
+$clase_write->write();
+echo "<br>";
+
 **/
+
+
+
 
 ?>
 
